6d9c6f618eeeaff71c98dac2c0a2766ddb2d7dda
Afham Faiz <hussain.afham.f@gmail.com>
2026-06-24 16:50:59 +0500

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+integer_type = 10
+float_type = 10.1
+bool_type = True
+string_type = "Ten"
+list_type = [10, 10, 10]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
